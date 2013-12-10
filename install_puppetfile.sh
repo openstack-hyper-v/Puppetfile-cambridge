dbfd6fe811bda58d61939ba0a8826ac08eb814da
primeministerp <primeministerpete@gmail.com>
2013-12-09 23:50:34 -0500

    Regenerating Puppetfile

--- install_puppetfile.sh
+++ install_puppetfile.sh
@@ -4,13 +4,13 @@
 echo "Building the Puppetfile"
 cat src/header.txt > Puppetfile
 echo "Creating Puppetfile Settings"
-cat src/settings.puppetfile >> Puppetfile
+cat src/settings.modules >> Puppetfile
 echo "Adding PuppetLabs modules to Puppetfile"
-cat src/puppetlabs.puppetfile >> Puppetfile
+cat src/puppetlabs.modules >> Puppetfile
 echo "Adding openstack-hyper-v modules to Puppetfile"
-cat src/openstack-hyper-v.puppetfile >> Puppetfile
+cat src/openstack-hyper-v.modules >> Puppetfile
 echo "Adding all other modules to Puppetfile"
-cat src/misc.puppetfile >> Puppetfile
+cat src/misc.modules >> Puppetfile
 echo "commiting changes back to git"
 git add *
 git commit -m "Regenerating Puppetfile"
