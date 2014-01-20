523568110efc33eee2fb4cc0e23126bc57d6d96f
primeministerp <primeministerpete@gmail.com>
2014-01-20 12:06:06 -0500

    adding repos to support sensu

---
+++ update_puppetfile_only.sh
@@ -0,0 +1,17 @@
+#
+# This validates the formatting of the Puppetfile file
+#
+echo "Removing Outdated Puppetfile"
+rm -rf /etc/puppet/Puppetfile
+echo "Building the Puppetfile"
+cat src/header.txt > Puppetfile
+echo "Creating Puppetfile Settings"
+cat src/settings.puppetfile >> Puppetfile
+echo "Adding PuppetLabs modules to Puppetfile"
+cat src/puppetlabs.modules >> Puppetfile
+echo "Adding openstack-hyper-v modules to Puppetfile"
+cat src/openstack-hyper-v.modules >> Puppetfile
+echo "Adding all other modules to Puppetfile"
+cat src/misc.modules >> Puppetfile
+echo "Installing New Puppet file in /etc/puppet/Puppetfile"
+ln -f -s `pwd`/Puppetfile /etc/puppet/Puppetfile
