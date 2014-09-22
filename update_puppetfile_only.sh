012096de2d2983691e2c33787ffa353a3340eb07
Peter J. Pouliot <peter@pouliot.net>
2014-09-22 13:31:47 -0400

    aligning with install puppetfile

--- update_puppetfile_only.sh
+++ update_puppetfile_only.sh
@@ -7,17 +7,8 @@ echo "Building the Puppetfile"
 cat src/header.txt > Puppetfile
 echo "Creating Puppetfile Settings"
 cat src/settings.puppetfile >> Puppetfile
+
 echo "Adding PuppetLabs modules to Puppetfile"
-cat src/puppetlabs.modules >> Puppetfile
-echo "Adding openstack-hyper-v modules to Puppetfile"
-cat src/openstack-hyper-v.modules >> Puppetfile
-echo "Adding StackForge modules to Puppetfile"
-cat src/stackforge.modules >> Puppetfile
-echo "Adding rismoney's windows modules to Puppetfile"
-cat src/rismoney.modules >> Puppetfile
-echo "Adding insentia modules to Puppetfile"
-cat src/insentia.modules >> Puppetfile
-echo "Adding all other modules to Puppetfile"
-cat src/misc.modules >> Puppetfile
+cat src/*.modules >> Puppetfile
 echo "Installing New Puppet file in /etc/puppet/Puppetfile"
 ln -f -s `pwd`/Puppetfile /etc/puppet/Puppetfile
